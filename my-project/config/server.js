bcff1699af3008ad6ab462c926cc3075e513cd61
artjitt9611 <jittaphon_jankham@cmu.ac.th>
2021-09-21 13:01:34 +0700

    edit

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '18598722e10d7f71bc5d92b70329fce4'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8a8834ee-751a-4db2-93b5-9dfa47197a04'
+};
